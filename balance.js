539e8aea05c74744fc348ebbebb398235f30e5dc
Tttmods <travisvarnes79@gmail.com>
2025-09-04 20:40:37 -0400

    Delete balance.js

--- balance.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "balance",
-  description: "Check balance",
-  async run({ message, db }) {
-    const row = db.prepare('SELECT coins FROM users WHERE id=?').get(message.author.id);
-    const coins = (row && row.coins) ? row.coins : 0;
-    message.reply('💰 Balance: ' + coins);
-  }
-};
