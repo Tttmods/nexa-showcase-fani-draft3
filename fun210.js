1d93abdb645521fc854ff6b5f9d926f17448483d
Tttmods <travisvarnes79@gmail.com>
2025-09-04 20:42:42 -0400

    Delete fun210.js

--- fun210.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "fun210",
-  description: "Fun filler command #210",
-  async run({ message }) {
-    message.reply("This is fun command #210 — NexaEverything © NexaStudios");
-  }
-};
